05dc0aa4359b37ad0fd97ef2335556496797d3e0
VidushAgarwal <59796609+VidushAgarwal@users.noreply.github.com>
2021-10-28 00:45:58 +0530

    Added new feature
    Added feature to  give area for a triangle with all different sides

--- Q1.c
+++ Q1.c
@@ -1,7 +1,7 @@
+#include <math.h>
 #include <stdio.h>
 void main()
 {
-    printf("Vidush Agarwal 1000014277\n");
     float base, height, area;
     printf("Enter base\n");
     scanf("%f", &base);
@@ -9,4 +9,13 @@ void main()
     scanf("%f", &height);
     area = base * height / 2;
     printf("Area : %f", area);
+    printf("Enter dimensions for triangle with different sides");
+    int a,b,c,s;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    s=(a+b+c)/2;
+    double are;
+    are=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("Area : %f", are);
 }
