0526cd639426ca8478b7af92adac80df3a96cb0e
shanky3939 <83959915+shanky3939@users.noreply.github.com>
2021-10-28 02:06:33 +0530

    added comments in the code and made some major changes to this code now its working fine without showing errors

--- Q1.c
+++ Q1.c
@@ -3,6 +3,8 @@
 void main()
 {
 
+    // code to find out the area of triangle 
+    
     float base, height, area;
     printf("Enter base\n");
     scanf("%f", &base);
@@ -10,17 +12,17 @@ void main()
     scanf("%f", &height);
     area = base * height / 2;
     printf("Area : %f", area);
-
-    printf("Sum of base and height is:- %f %f ",height+base);
-=======
+    
+    //code  to find area of triangle with different method  or we can say to find out the area of equilateral triangle
+    printf("Sum of base and height is:- %f ",height+base);
     printf("Enter dimensions for triangle with different sides");
-    int a,b,c,s;
+    float a,b,c,s;
     scanf("%f", &a);
     scanf("%f", &b);
     scanf("%f", &c);
     s=(a+b+c)/2;
-    double are;
-    are=sqrt(s*(s-a)*(s-b)*(s-c));
-    printf("Area : %f", are);
+    float area2;
+    area2=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("Area : %f", area2);
 
 }
