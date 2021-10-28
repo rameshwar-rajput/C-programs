45dcc97eb32e07c94fd0299522c82ffb725cdee5
Kshitij <79253490+Kshitij1818@users.noreply.github.com>
2021-10-29 00:23:05 +0530

    Create sum_of_power.c
    To find sum of first 10 numbers with each term raised to power entered.

---
+++ sum_of_power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    printf("Kshitij Gupta\n");
+    int n, sum = 0, i;
+    printf("Enter power of values whose sum is needed to be found : ");
+    scanf("%d", &n);
+    for (i = 1; i <=10; i++)
+        sum = sum + pow(i,n);
+    printf("Sum of first 10 number with power %d is %d\n", n, sum);
+}
